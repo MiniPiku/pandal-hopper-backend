0b5c6d56f3a8626548e60f49a5f9f557a91c7508
MiniPiku <21parthib2006@gmail.com>
2025-09-11 01:39:40 +0530

    implemented shorted path from user->metro

--- src/main/java/org/minipiku/pandalhopperv2/Repository/PandalRepository.java
+++ src/main/java/org/minipiku/pandalhopperv2/Repository/PandalRepository.java
@@ -2,6 +2,8 @@ package org.minipiku.pandalhopperv2.Repository;
 
 import org.minipiku.pandalhopperv2.Entity.Pandal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PandalRepository extends JpaRepository<Pandal, String> {
+    List<Pandal> findByZone(String zone);
 }
