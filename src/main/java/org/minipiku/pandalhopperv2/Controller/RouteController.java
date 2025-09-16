7f91378d2969482d8286e0ea55ee03a323ea599a
MiniPiku <21parthib2006@gmail.com>
2025-09-16 13:47:31 +0530

    final fixes

--- src/main/java/org/minipiku/pandalhopperv2/Controller/RouteController.java
+++ src/main/java/org/minipiku/pandalhopperv2/Controller/RouteController.java
@@ -5,13 +5,10 @@ import org.minipiku.pandalhopperv2.DTOs.RouteRequestDTO;
 import org.minipiku.pandalhopperv2.DTOs.RouteResponseDTO;
 import org.minipiku.pandalhopperv2.Service.RoutingService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/route")
 @RequiredArgsConstructor
