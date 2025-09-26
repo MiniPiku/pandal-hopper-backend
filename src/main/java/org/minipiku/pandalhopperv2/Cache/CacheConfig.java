819daac0b0c86ff26424eb2fa5c134e83c518d7e
MiniPiku <21parthib2006@gmail.com>
2025-09-26 23:26:54 +0530

    implement rest of the cache in PandalService

--- src/main/java/org/minipiku/pandalhopperv2/Cache/CacheConfig.java
+++ src/main/java/org/minipiku/pandalhopperv2/Cache/CacheConfig.java
@@ -1,15 +1,11 @@
 package org.minipiku.pandalhopperv2.Cache;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.minipiku.pandalhopperv2.DTOs.MetroPandalDTO.PandalDTO;
-import org.minipiku.pandalhopperv2.DTOs.MetroPandalDTO.SimplePandalDTO;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
@@ -27,7 +23,7 @@ public class CacheConfig {
                 new Jackson2JsonRedisSerializer<>(List.class);
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofMinutes(10))
+                .entryTtl(Duration.ofMinutes(60))
                 .disableCachingNullValues()
                 .serializeValuesWith(
                         RedisSerializationContext.SerializationPair.fromSerializer(serializer)
